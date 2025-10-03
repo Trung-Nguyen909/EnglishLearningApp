bc2621a1eec95e0abf557a750a3b8c0fa864fb15
tienhienn <huynhletienhien@gmail.com>
2025-10-03 23:31:51 +0700

    lam xong man hinh splash de vo view login

--- EnglishLearningApp/app/src/main/java/com/example/englishlearningapp/SplashActivity.java
+++ EnglishLearningApp/app/src/main/java/com/example/englishlearningapp/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.englishlearningapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Delay 3 giây rồi chuyển sang MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // đóng SplashActivity để không quay lại khi bấm Back
+            }
+        }, 1500); // 3000 ms = 3 giây
     }
 }
