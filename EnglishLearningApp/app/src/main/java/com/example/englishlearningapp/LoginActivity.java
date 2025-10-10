6f8f1c29c8b9fdda31af8712e6be781131df658c
TruongVanTrang <vantrang04042005@hotmail.com>
2025-10-10 18:20:41 +0700

    trang up code GD2

--- EnglishLearningApp/app/src/main/java/com/example/englishlearningapp/LoginActivity.java
+++ EnglishLearningApp/app/src/main/java/com/example/englishlearningapp/LoginActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btnDangKy;
+    Button btnDangKy, btnDangNhapTC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +23,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnDangNhapTC = findViewById(R.id.btnDangNhapTC);
+        btnDangNhapTC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ExerciseProgressActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
 
