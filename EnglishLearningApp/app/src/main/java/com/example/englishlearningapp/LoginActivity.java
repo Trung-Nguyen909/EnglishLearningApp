45ae77eb38cdaabebdff0962bcea841f58cdf5c1
tuankhanhh <ttuankhanh4@gmail.com>
2025-11-09 17:16:06 +0700

    up layout login

--- EnglishLearningApp/app/src/main/java/com/example/englishlearningapp/LoginActivity.java
+++ EnglishLearningApp/app/src/main/java/com/example/englishlearningapp/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.englishlearningapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,8 +8,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
     }
 }
-
